02584aefd7d88fa3682bd3a9b23a8450f0b265c9
saurabh sharma <81613615+saurabh7310@users.noreply.github.com>
2023-06-04 13:48:44 +0530

    Update 03_Question.py

--- Assessments/Python/03_Question.py
+++ Assessments/Python/03_Question.py
@@ -6,15 +6,34 @@ def download_and_convert_to_excel(url, output_file):
     response = requests.get(url)
     data = response.json()
 
-    # Convert the data to a structured format
+    # Extract the required attributes from each Pokemon entry
     structured_data = []
-    for item in data:
-        structured_item = {
-            'Name': item['name'],
-            'Age': item['age'],
-            'Email': item['email']
+    for pokemon in data['pokemon']:
+        attributes = {
+            'ID': pokemon['id'],
+            'Number': pokemon['num'],
+            'Name': pokemon['name'],
+            'Image URL': pokemon['img'],
+            'Type': ', '.join(pokemon['type']),
+            'Height': pokemon['height'],
+            'Weight': pokemon['weight'],
+            'Candy': pokemon.get('candy', ''),
+            'Candy Count': pokemon.get('candy_count', ''),
+            'Egg Distance (km)': pokemon.get('egg', ''),
+            'Spawn Chance (%)': pokemon.get('spawn_chance', ''),
+            'Avg Spawns': pokemon.get('avg_spawns', ''),
+            'Spawn Time': pokemon.get('spawn_time', ''),
+            'Weaknesses': ', '.join(pokemon.get('weaknesses', [])),
+            'Next Evolution': ', '.join([
+                f"{evolution['num']}: {evolution['name']}"
+                for evolution in pokemon.get('next_evolution', [])
+            ]),
+            'Previous Evolution': ', '.join([
+                f"{evolution['num']}: {evolution['name']}"
+                for evolution in pokemon.get('prev_evolution', [])
+            ])
         }
-        structured_data.append(structured_item)
+        structured_data.append(attributes)
 
     # Create a Pandas DataFrame from the structured data
     df = pd.DataFrame(structured_data)
@@ -25,6 +44,6 @@ def download_and_convert_to_excel(url, output_file):
     print(f"Data has been successfully converted and saved to '{output_file}'.")
 
 # Example usage
-url = "https://api.example.com/data"
-output_file = "structured_data.xlsx"
+url = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json"
+output_file = "pokemon_data.xlsx"
 download_and_convert_to_excel(url, output_file)
