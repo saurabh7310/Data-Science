ce0405ad621c66bf0e70bc4e37b6e4bd2cbaddd3
saurabh sharma <81613615+saurabh7310@users.noreply.github.com>
2023-06-04 13:38:59 +0530

    Add files via upload

---
+++ Assessments/Python/02_Question.py
@@ -0,0 +1,22 @@
+def searchInsert(nums, target):
+    left = 0
+    right = len(nums) - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        
+        if nums[mid] == target:
+            return mid
+        
+        if nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return left
+
+# Example usage
+nums = [1, 3, 5, 6]
+target = 5
+index = searchInsert(nums, target)
+print("Output:", index)
